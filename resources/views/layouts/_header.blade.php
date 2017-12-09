f681fe491edfb73bdd36de0f0e48f459f4dab804
zcdll <qinsword521@gmail.com>
2017-12-09 10:38:44 +0800

    User can upload avatar

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -29,7 +29,8 @@
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                         <span class="user-avatar pull-left" style="margin-right: 8px; margin-top:-5px;">
-                            <img src="{{ $user->avatar }}"
+                            {{--<img src=""--}}
+                            <img src="{{ Auth::user()->avatar }}"
                                  class="img-responsive" width="30px" height="30px">
                         </span>
                             {{ Auth::user()->name }} <span class="caret"></span>
