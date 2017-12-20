ac7f930dfa8ea83ffd4662aff23326dc96c728cb
zcdll <qinsword521@gmail.com>
2017-12-20 23:15:58 +0800

    Admin can manage site settings

--- config/administrator.php
+++ config/administrator.php
@@ -63,10 +63,12 @@ return array(
             'users',
             'roles',
             'permissions',
-        ],'内容管理' => [
+        ], '内容管理' => [
             'categories',
             'topics',
             'replies',
+        ], '站点管理' => [
+            'settings.site',
         ],
     ],
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,8 +9,9 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title','LaraBBS') - try Laravel</title>
-    <meta name="description" content="@yield('description', 'try Laravel')">
+    <title>@yield('title','LaraBBS') - {{ setting('site_name', 'LaraBBS') }}</title>
+    <meta name="description" content="@yield('description', setting('seo_description', 'LaraBBS 爱好者社区'))">
+    <meta name="keyword" content="@yield('keyword', setting('seo_keyword', 'LaraBBS,社区,论坛,开发者论坛'))">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
